012a5bb7e816009f0554311089b9b8e92bb32fbf
unknown <dekunbifola100@gmail.com>
2021-02-11 20:27:46 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.get("/api/timestamp/:date", (req, res) => {
+  const { date } = req.params;
+
+  const newDateValue = new Date(Number(date));
+  res.send(
+    String(newDateValue) !== "Invalid Date"
+      ? { unix: Number(date), utc: String(newDateValue) }
+      : { utc: String(newDateValue) }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+})
